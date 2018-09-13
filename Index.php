ba23035de6dad25e3bb3467196919e08bee98d28
ExoDragon <marcus.terlaak@gmail.com>
2018-09-13 18:51:21 +0200

    Startup Program
    Beginning the Stockmarket Program

---
+++ Index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="icon" href="Images/Graph.png">
+    <link rel="stylesheet" href="Style/Style.css" type="text/css"/>
+    <title> IB3A_StockMarket </title>
+</head>
+
+<body>
+
+    <header>
+
+        <ul>
+            <li></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">Market</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li class="right"><a href="#Account">Login</a></li>
+            <li class="right"><a href="#Search"><span class="glyphicon glyphicon-search full"></a></li></span>
+        </ul>
+
+    </header>
+
+    <footer>
+    </footer>
+
+
+</body>
+
+</html>
